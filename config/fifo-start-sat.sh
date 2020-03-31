e7a270ac9f2289a1e1dd587a95e6fded78898e8f
RSAP <46652797+DABodr@users.noreply.github.com>
2020-04-01 01:33:53 +0200

    Add files via upload

---
+++ config/fifo-start-sat.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkfifo /tmp/soxmelody.fifo
+mkfifo /tmp/padmelody.fifo
+
+mkfifo /tmp/soxfun.fifo
+mkfifo /tmp/padfun.fifo
+
+mkfifo /tmp/soxnova.fifo
+mkfifo /tmp/padnova.fifo
+
+mkfifo /tmp/soxfip.fifo
+mkfifo /tmp/soxfip.fifo
+
